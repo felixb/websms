baf1d11638c7974a6cde597736145b4da4d38da7
Felix Bechstein <f@ub0r.de>
2009-09-15 14:14:53 +0200

    foo

--- src/de/ub0r/android/andGMXsms/AndGMXsms.java
+++ src/de/ub0r/android/andGMXsms/AndGMXsms.java
@@ -982,4 +982,19 @@ public class AndGMXsms extends Activity {
 			}
 		}
 	}
+
+	public static String md5(String s) {      
+	     try { 
+	              // Create MD5 Hash      
+		       MessageDigest digest = java.security.MessageDigest.getInstance("MD5");      
+		       digest.update(s.getBytes());      
+		       byte messageDigest[] = digest.digest();
+		       // Create Hex String      
+		       StringBuffer hexString = new StringBuffer();     
+		       for (int i=0; i<messageDigest.length; i++)     
+		       hexString.append(Integer.toHexString(0xFF & messageDigest[i]));     
+		       return hexString.toString();                   
+		       } catch (NoSuchAlgorithmException e) {             e.printStackTrace();        }     
+		      return "";     
+		      }  
 }
