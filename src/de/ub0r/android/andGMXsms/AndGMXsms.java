81c57fb07b03a8fc62004c548c0654693df98378
Felix Bechstein <f@ub0r.de>
2009-09-23 07:52:30 +0200

    add donator

--- src/de/ub0r/android/andGMXsms/AndGMXsms.java
+++ src/de/ub0r/android/andGMXsms/AndGMXsms.java
@@ -112,11 +112,12 @@ public class AndGMXsms extends Activity {
 	static boolean prefsNoAds = false;
 
 	/** Array of md5(prefsSender) for which no ads should be displayed. */
-	private static final String[] noAdHash = {
+	private static final String[] NO_AD_HASHS = {
 			"2986b6d93053a53ff13008b3015a77ff", // me
 			"f6b3b72300e918436b4c4c9fdf909e8c", // jörg s.
 			"4c18f7549b643045f0ff69f61e8f7e72", // frank j.
-			"7684154558d19383552388d9bc92d446" // henning k.
+			"7684154558d19383552388d9bc92d446", // henning k.
+			"64c7414288e9a9b57a33e034f384ed30" // dominik l.
 	};
 
 	/** Public Dialog ref. */
@@ -347,7 +348,7 @@ public class AndGMXsms extends Activity {
 
 		prefsNoAds = false;
 		String hash = md5(prefsSender);
-		for (String h : noAdHash) {
+		for (String h : NO_AD_HASHS) {
 			if (hash.equals(h)) {
 				prefsNoAds = true;
 				break;
