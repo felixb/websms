674793201c0a61ba00c7f77fe3567224f183e367
Felix Bechstein <f@ub0r.de>
2009-10-18 22:41:47 +0200

    fix o2 captcha

--- src/de/ub0r/android/andGMXsms/Connector.java
+++ src/de/ub0r/android/andGMXsms/Connector.java
@@ -29,7 +29,6 @@ import java.util.ArrayList;
 
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
@@ -224,14 +223,15 @@ public abstract class Connector extends AsyncTask<String, Boolean, Boolean> {
 	 *            post data
 	 * @param userAgent
 	 *            user agent
+	 * @param referer
+	 *            referer
 	 * @return the connection
 	 * @throws IOException
-	 * @throws ClientProtocolException
 	 */
 	protected static HttpResponse getHttpClient(final String url,
 			final ArrayList<Cookie> cookies,
-			final ArrayList<BasicNameValuePair> postData, final String userAgent)
-			throws ClientProtocolException, IOException {
+			final ArrayList<BasicNameValuePair> postData,
+			final String userAgent, final String referer) throws IOException {
 		HttpClient client = new DefaultHttpClient();
 		HttpRequestBase request;
 		if (postData == null) {
@@ -240,7 +240,12 @@ public abstract class Connector extends AsyncTask<String, Boolean, Boolean> {
 			request = new HttpPost(url);
 			((HttpPost) request).setEntity(new UrlEncodedFormEntity(postData));
 		}
-		request.setHeader("User-Agent", userAgent);
+		if (referer != null) {
+			request.setHeader("Referer", referer);
+		}
+		if (userAgent != null) {
+			request.setHeader("User-Agent", userAgent);
+		}
 
 		if (cookies != null && cookies.size() > 0) {
 			CookieSpecBase cookieSpecBase = new BrowserCompatSpec();
--- src/de/ub0r/android/andGMXsms/ConnectorO2.java
+++ src/de/ub0r/android/andGMXsms/ConnectorO2.java
@@ -127,7 +127,7 @@ public class ConnectorO2 extends Connector {
 		try { // get Connection
 			ArrayList<Cookie> cookies = new ArrayList<Cookie>();
 			HttpResponse response = getHttpClient(URLS[operator][0], cookies,
-					null, TARGET_AGENT);
+					null, TARGET_AGENT, null);
 			int resp = response.getStatusLine().getStatusCode();
 			if (resp != HttpURLConnection.HTTP_OK) {
 				AndGMXsms.sendMessage(AndGMXsms.MESSAGE_LOG, AndGMXsms.me
@@ -151,7 +151,7 @@ public class ConnectorO2 extends Connector {
 					AndGMXsms.prefsPasswordO2));
 			postData.add(new BasicNameValuePair("_eventId", "login"));
 			response = getHttpClient(URLS[operator][1], cookies, postData,
-					TARGET_AGENT);
+					TARGET_AGENT, URLS[operator][0]);
 			postData = null;
 			resp = response.getStatusLine().getStatusCode();
 			if (resp != HttpURLConnection.HTTP_OK) {
@@ -165,10 +165,11 @@ public class ConnectorO2 extends Connector {
 			if (resp == cookies.size()) {
 				AndGMXsms.sendMessage(AndGMXsms.MESSAGE_LOG,
 						R.string.log_error_pw);
+				htmlText = stream2String(response.getEntity().getContent());
 				return false;
 			}
 			response = getHttpClient(URLS[operator][2], cookies, null,
-					TARGET_AGENT);
+					TARGET_AGENT, URLS[operator][1]);
 			resp = response.getStatusLine().getStatusCode();
 			if (resp != HttpURLConnection.HTTP_OK) {
 				AndGMXsms.sendMessage(AndGMXsms.MESSAGE_LOG, AndGMXsms.me
@@ -179,7 +180,7 @@ public class ConnectorO2 extends Connector {
 			updateCookies(cookies, response.getAllHeaders(), URLS[operator][2]);
 
 			response = getHttpClient(URLS[operator][3], cookies, null,
-					TARGET_AGENT);
+					TARGET_AGENT, URLS[operator][2]);
 			resp = response.getStatusLine().getStatusCode();
 			if (resp != HttpURLConnection.HTTP_OK) {
 				AndGMXsms.sendMessage(AndGMXsms.MESSAGE_LOG, AndGMXsms.me
@@ -220,7 +221,7 @@ public class ConnectorO2 extends Connector {
 				st = null;
 
 				response = getHttpClient(URLS[operator][4], cookies, postData,
-						TARGET_AGENT);
+						TARGET_AGENT, URLS[operator][3]);
 				postData = null;
 				resp = response.getStatusLine().getStatusCode();
 				if (resp != HttpURLConnection.HTTP_OK) {
