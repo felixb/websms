16375cfe0d5b81657b3f673c84ceaac97b9d8139
Felix Bechstein <f@ub0r.de>
2009-11-14 12:44:14 +0100

    replace donate dialog

--- src/de/ub0r/android/andGMXsms/WebSMS.java
+++ src/de/ub0r/android/andGMXsms/WebSMS.java
@@ -815,29 +815,48 @@ public class WebSMS extends Activity implements OnClickListener,
 	@Override
 	protected final Dialog onCreateDialog(final int id) {
 		Dialog d;
+		AlertDialog.Builder builder;
 		switch (id) {
 		case DIALOG_DONATE:
-			d = new Dialog(this);
-			d.setContentView(R.layout.donate);
-			d.setTitle(R.string.remove_ads);
-			Button button = (Button) d.findViewById(R.id.btn_donate);
-			button.setOnClickListener(new OnClickListener() {
-				public void onClick(final View view) {
-					final Intent in = new Intent(Intent.ACTION_SEND);
-					in.putExtra(Intent.EXTRA_EMAIL, new String[] {
-							WebSMS.this.getString(R.string.donate_mail), "" });
-					// FIXME: "" is a k9 hack.
-					in.putExtra(Intent.EXTRA_TEXT, md5(WebSMS.prefsSender));
-					in.putExtra(Intent.EXTRA_SUBJECT, WebSMS.this
-							.getString(R.string.app_name)
-							+ " "
-							+ WebSMS.this.getString(R.string.donate_subject));
-					in.setType("text/plain");
-					WebSMS.this.startActivity(in);
-					WebSMS.this.dismissDialog(DIALOG_DONATE);
-				}
-			});
-			return d;
+			builder = new AlertDialog.Builder(this);
+			builder.setTitle(R.string.remove_ads);
+			builder.setMessage(R.string.postdonate);
+			builder.setPositiveButton(R.string.send_,
+					new DialogInterface.OnClickListener() {
+						public void onClick(final DialogInterface dialog,
+								final int which) {
+							final Intent in = new Intent(Intent.ACTION_SEND);
+							in
+									.putExtra(
+											Intent.EXTRA_EMAIL,
+											new String[] {
+													WebSMS.this
+															.getString(R.string.donate_mail),
+													"" });
+							// FIXME: "" is a k9 hack.
+							in.putExtra(Intent.EXTRA_TEXT, WebSMS
+									.md5(WebSMS.prefsSender));
+							in
+									.putExtra(
+											Intent.EXTRA_SUBJECT,
+											WebSMS.this
+													.getString(R.string.app_name)
+													+ " "
+													+ WebSMS.this
+															.getString(R.string.donate_subject));
+							in.setType("text/plain");
+							WebSMS.this.startActivity(in);
+							dialog.dismiss();
+						}
+					});
+			builder.setNegativeButton(android.R.string.cancel,
+					new DialogInterface.OnClickListener() {
+						public void onClick(final DialogInterface dialog,
+								final int which) {
+							dialog.cancel();
+						}
+					});
+			return builder.create();
 		case DIALOG_ABOUT:
 			d = new Dialog(this);
 			d.setContentView(R.layout.about);
@@ -850,7 +869,7 @@ public class WebSMS extends Activity implements OnClickListener,
 			d.setTitle(R.string.help_);
 			return d;
 		case DIALOG_UPDATE:
-			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder = new AlertDialog.Builder(this);
 			builder.setTitle(R.string.changelog_);
 			final String[] changes = this.getResources().getStringArray(
 					R.array.updates);
