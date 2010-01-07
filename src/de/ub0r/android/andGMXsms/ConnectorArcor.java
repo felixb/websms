04e095138aeb6444a79aa58a8764b53f830ec7d1
lado <herrlado@gmail.com>
2010-01-07 15:35:13 +0100

    doc

--- src/de/ub0r/android/andGMXsms/ConnectorArcor.java
+++ src/de/ub0r/android/andGMXsms/ConnectorArcor.java
@@ -21,7 +21,6 @@ import org.apache.http.message.BasicNameValuePair;
 
 import android.widget.AutoCompleteTextView.Validator;
 
-
 /**
  * 
  * Connector for arcor.de free sms / payed sms
@@ -32,6 +31,9 @@ import android.widget.AutoCompleteTextView.Validator;
  */
 public class ConnectorArcor extends Connector {
 
+	/**
+	 * Extract free and payed sms count from response//TODO try match only substring(performance) or even try if arcor web server support partial content
+	 */
 	private final static Pattern BALANCE_MATCH_PATTERN = Pattern
 			.compile("<td.+class=\"txtRed\".*?<b>(\\d{1,2})</b>.+<td.+class=\"txtRed\".*?<b>(\\d{1,})</b>");
 
@@ -39,16 +41,19 @@ public class ConnectorArcor extends Connector {
 
 	private final static String MATCH_LOGIN_SUCCESS = "logout.jsp";
 
-	/** Last access um to reuse httpsession */
-	//private long lastAccess = 0;
-
 	private HttpClient client = null;
 
 	// TODO share this. Make it Configurable clobal and local
 	private static final String FAKE_USER_AGENT = "Mozilla/5.0 (Windows; U;"
 			+ " Windows NT 5.1; de; rv:1.9.0.9) Gecko/2009040821"
 			+ " Firefox/3.0.9 (.NET CLR 3.5.30729)";
-
+	
+	/**
+	 * needed urls
+	 * 
+	 * @author lado
+	 *
+	 */
 	private interface URL {
 		public static final String Login = "https://www.arcor.de/login/login.jsp";
 		public static final String Sms = "https://www.arcor.de/ums/ums_neu_sms.jsp";
@@ -58,16 +63,16 @@ public class ConnectorArcor extends Connector {
 		super(u, p, con);
 	}
 
+	/**
+	 * Login to arcor
+	 * @return
+	 * @throws WebSMSException
+	 */
 	protected boolean login() throws WebSMSException {
 		try {
 			HttpClient client = getHttpClient();
 			HttpPost request = createPOST(URL.Login, getLoginPost());
 			HttpResponse response = client.execute(request);
-			//if (response.getStatusLine().getStatusCode() == 302) {
-				//updateBalance();
-				//touchLastAccess();
-				//return true;
-			//}
 			String content = stream2str(response.getEntity().getContent());
 			if (content.indexOf(MATCH_LOGIN_SUCCESS) > 0) {
 				return true;
@@ -81,13 +86,23 @@ public class ConnectorArcor extends Connector {
 		}
 		return false;
 	}
-	
-	private HttpResponse execute(HttpRequestBase request) throws Exception{
-			return getHttpClient().execute(request);
+
+	/**
+	 * Simple execute a Http Request
+	 * @param request
+	 * @return
+	 * @throws Exception
+	 */
+	private HttpResponse execute(HttpRequestBase request) throws Exception {
+		return getHttpClient().execute(request);
 	}
 
-	protected boolean updateBalance() throws WebSMSException {
-		try {//TODO better try catch
+	/**
+	 * @return
+	 * @throws WebSMSException
+	 */
+	private boolean updateBalance() throws WebSMSException {
+		try {// TODO better try catch
 
 			HttpResponse response = execute(createGET(URL.Sms, null));
 
@@ -95,14 +110,15 @@ public class ConnectorArcor extends Connector {
 			String content = stream2str(response.getEntity().getContent());
 
 			Matcher m = BALANCE_MATCH_PATTERN.matcher(content);
-			if (m.find() == true) {//TODO damn, this code does not work on my device?
-				String f = m.group(1);//this ist free sms count
-				String p = m.group(2);//this ist payed sms count
+			if (m.find() == true) {// TODO damn, this code does not work on my
+									// device?
+				String f = m.group(1);// this ist free sms count
+				String p = m.group(2);// this ist payed sms count
 				System.out.println(f);
 				System.out.println(m);
-				pushMessage(WebSMS.MESSAGE_FREECOUNT, f + "f, "+ p + "$");
-			}else {
-				
+				pushMessage(WebSMS.MESSAGE_FREECOUNT, f + "f, " + p + "$");
+			} else {
+
 			}
 			return true;
 		} catch (Exception ex) {
@@ -112,16 +128,26 @@ public class ConnectorArcor extends Connector {
 
 	}
 
-	protected boolean sendSms() throws WebSMSException {
-		try{
-		HttpResponse response = execute(createPOST(URL.Sms, getSmsPost()));
-		
-		return true;
-		}catch(Exception ex){
+	/**
+	 * @return
+	 * @throws WebSMSException
+	 */
+	private boolean sendSms() throws WebSMSException {
+		try {
+			HttpResponse response = execute(createPOST(URL.Sms, getSmsPost()));
+
+			return true;
+		} catch (Exception ex) {
 			return false;
 		}
 	}
 
+	/**
+	 * If not already created. create an instance of {@link HttpClient}.
+	 * This will be cached for a sending cycle as a member variable.
+	 * So login && send ||  login && updateBalance needs only one instance 
+	 * @return {@link HttpClient} instance
+	 */
 	private HttpClient getHttpClient() {
 		if (this.client == null) {
 			this.client = new DefaultHttpClient();
@@ -129,7 +155,11 @@ public class ConnectorArcor extends Connector {
 		return this.client;
 	}
 
-	protected ArrayList<BasicNameValuePair> getLoginPost() {
+	/**
+	 * This post data is needed for log in
+	 * @return
+	 */
+	private ArrayList<BasicNameValuePair> getLoginPost() {
 		ArrayList<BasicNameValuePair> post = new ArrayList<BasicNameValuePair>();
 		post.add(new BasicNameValuePair("user_name", this.user));
 		post.add(new BasicNameValuePair("password", this.password));
@@ -139,57 +169,79 @@ public class ConnectorArcor extends Connector {
 		post.add(new BasicNameValuePair("goto", ""));
 		return post;
 	}
-	
 
+	/**
+	 * These post data is needed for sending a sms
+	 * @return
+	 */
 	protected ArrayList<BasicNameValuePair> getSmsPost() {
 		ArrayList<BasicNameValuePair> post = new ArrayList<BasicNameValuePair>();
-		post.add(new BasicNameValuePair("empfaengerAn", this.to[0]));//TODO this is only test
-		post.add(new BasicNameValuePair("emailAdressen", WebSMS.prefsSender)); //TODO customize http://code.google.com/p/websmsdroid/issues/detail?id=42&colspec=ID%20Type%20Status%20Priority%20Product%20Component%20Owner%20Summary#c6
+		post.add(new BasicNameValuePair("empfaengerAn", this.to[0]));// TODO
+																		// this
+																		// is
+																		// only
+																		// test
+		post.add(new BasicNameValuePair("emailAdressen", WebSMS.prefsSender)); // TODO
+																				// customize
+																				// http://code.google.com/p/websmsdroid/issues/detail?id=42&colspec=ID%20Type%20Status%20Priority%20Product%20Component%20Owner%20Summary#c6
 		post.add(new BasicNameValuePair("nachricht", this.text));
-		post.add(new BasicNameValuePair("gesendetkopiesms", "on"));//TODO on/off see http://code.google.com/p/websmsdroid/issues/detail?id=42&colspec=ID%20Type%20Status%20Priority%20Product%20Component%20Owner%20Summary#c8
+		post.add(new BasicNameValuePair("gesendetkopiesms", "on"));// TODO
+																	// on/off
+																	// see
+																	// http://code.google.com/p/websmsdroid/issues/detail?id=42&colspec=ID%20Type%20Status%20Priority%20Product%20Component%20Owner%20Summary#c8
 		post.add(new BasicNameValuePair("firstVisitOfPage", "0"));
 		post.add(new BasicNameValuePair("part", "0"));
 		return post;
 	}
 
-	
-	private HttpGet createGET(String url,List<BasicNameValuePair> params){
-		if(params == null || params.isEmpty()){
+	/**
+	 *  Create and Prepare a Get Request. Set also an User-Agent
+	 *  
+	 * @param url
+	 * @param params
+	 * @return
+	 */
+	private HttpGet createGET(String url, List<BasicNameValuePair> params) {
+		if (params == null || params.isEmpty()) {
 			return new HttpGet(url);
 		}
 
 		StringBuilder sb = new StringBuilder();
 		sb.append(url).append("?");
-		for(BasicNameValuePair p : params){
+		for (BasicNameValuePair p : params) {
 			sb.append(p.getName()).append("=").append(p.getValue());
 		}
 		return new HttpGet(sb.toString());
 	}
-	
-	private HttpPost createPOST(String url, List<BasicNameValuePair> postData) throws UnsupportedEncodingException{
+
+	/**
+	 * Create and Prepare a Post Request. Set also an User-Agent
+	 * 
+	 * @param url
+	 * @param postData
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	private HttpPost createPOST(String url, List<BasicNameValuePair> postData)
+			throws UnsupportedEncodingException {
 		HttpPost post = new HttpPost(url);
 		post.setHeader("User-Agent", FAKE_USER_AGENT);
 		post.setEntity(new UrlEncodedFormEntity(postData));
 		return post;
 	}
-	
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected boolean sendMessage() throws WebSMSException {
 		return login() && sendSms();
 	}
 
-//	private void touchLastAccess() {
-	//	lastAccess = System.currentTimeMillis();
-	//}
 
-	//private boolean needLogin() {
-		//return (System.currentTimeMillis() - lastAccess) > 1000 * 60 * 10;// TODO
-																			// find out
-																			// session
-																			// timeout
-																			// for
-																			// arcor
-	//}
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected boolean updateMessages() throws WebSMSException {
 		return (login() && updateBalance());
