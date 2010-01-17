9068fe18781374a1d5f6d130d807c6681fe3c885
Felix Bechstein <f@ub0r.de>
2010-01-17 20:14:56 +0100

    do builddoc.sh - view http://nossl.ub0r.de/~f/websms/doc/ for results

--- builddoc.sh
+++ builddoc.sh
@@ -1,1 +1,5 @@
-javadoc de.ub0r.android $(find ../ -name \*.java)
+#! /bin/sh
+
+javadoc -quiet -d doc -author -version de.ub0r.android $(find . -name \*.java) > /dev/null 2> /dev/null
+cp -ar doc /home/flx/public_html/websms/
+
