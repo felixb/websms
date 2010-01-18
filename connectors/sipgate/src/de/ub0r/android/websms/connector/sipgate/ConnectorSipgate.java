c9495b28b56351f37778b11c1292c464f0b199e7
Felix Bechstein <f@ub0r.de>
2010-01-18 09:22:55 +0100

    fix prefs, fix packages

--- connectors/sipgate/src/de/ub0r/android/websms/connector/sipgate/ConnectorSipgate.java
+++ connectors/sipgate/src/de/ub0r/android/websms/connector/sipgate/ConnectorSipgate.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/>.
  */
-package de.ub0r.android.andGMXsms;
+package de.ub0r.android.websms.connector.sipgate;
 
 import java.io.Serializable;
 import java.util.Hashtable;
