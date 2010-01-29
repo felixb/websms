38160c8ec7c709e157afb9251d933d571c2a160a
Felix Bechstein <f@ub0r.de>
2010-01-29 15:35:17 +0100

    fix postDeploy.sh

--- postDeploy.sh
+++ postDeploy.sh
@@ -11,4 +11,6 @@ for f in $(find "${p}" -name \*java) ; do
 	sed -e 's:///*Log.d:Log.d:' -i $f
 done
 
-sed -e 's/android:debuggable="false"/android:debuggable="true"/' -i AndroidManifest.xml
+for f in $(find . -name AndroidManifest.xml) ; do
+	sed -e 's/android:debuggable="false"/android:debuggable="true"/' -i $f
+done
