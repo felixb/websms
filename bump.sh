4e2b069be0e5602bba7ac9e2f9269d6e696fd3d6
Felix Bechstein <f@ub0r.de>
2010-04-20 06:17:17 +0200

    mod bump.sh

--- bump.sh
+++ bump.sh
@@ -10,7 +10,7 @@ else
 fi
 
 v=${1}
-vv=$(echo ${v}0000 | tr -d . | head -c4 | sed -e 's:^0::g')
+vv=$(($(grep versionCode AndroidManifest.xml | cut -d\" -f2) + 1))
 
 if [ -n "$2" ] ; then
 	vn="$v $2"
